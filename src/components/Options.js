ef8376a866c154dc597548f691bcaa3c42aa1b5e
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-07-17 14:40:57 +0300

    Add answer state management and update components to handle user answers

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,26 @@
-export default function Options({question}) {
-return (
-  <div className="options">
-    {question.options.map((option) => (
-      <button className="btn btn-option" key={option}>
-        {option}
-      </button>
-    ))}
-  </div>
-);
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswer = answer !== null;
+
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswer
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswer}
+          onClick={() => {
+            dispatch({ type: "newAnswer", payload: index });
+          }}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 }
