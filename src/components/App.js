e743e18d1df7f002315a520fba594a4945a88085
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-07-18 15:10:05 +0300

    Add restart functionality to FinishScreen and update reducer to reset state

--- src/components/App.js
+++ src/components/App.js
@@ -61,6 +61,12 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
 
     default:
       throw new Error("Action unKnown");
@@ -119,6 +125,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
